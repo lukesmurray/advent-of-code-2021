fde1cd0528320011cf87490d362425806ba8924c
Luke Murray <lukepigeonmail@gmail.com>
2021-12-01 12:37:19 -0500

    shorter day 1

--- day1/part2/main.py
+++ day1/part2/main.py
@@ -1,3 +1,2 @@
 d=[int(l)for l in open("input.txt").readlines()]
-e=[sum(d[i-2:i+1])for i in range(2,len(d))]
-print(sum([int(e[i])>int(e[i-1])for i in range(len(e))]))
+print(sum([int(d[i])>int(d[i-3])for i in range(3, len(d))]))
