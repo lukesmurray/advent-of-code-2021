b9d735b050764f13af6b5c0f0903f6b63f73abfe
Luke Murray <lukepigeonmail@gmail.com>
2021-12-02 10:19:41 -0500

    day 2 part 2 shorter

--- day2/part2/main.py
+++ day2/part2/main.py
@@ -1,3 +1,4 @@
-x,y,a,d=0,0,0,[[t,int(u)]for t,u in[v.split()for v in open('i')]]
+x=y=a=0
+d=[[t,int(u)]for t,u in[v.split()for v in open('i')]]
 for t,u in d:x+=u*(t=="forward");y+=a*u*(t=="forward");a+=u*(t=="down");a-=u*(t=="up")
 print(x*y)
