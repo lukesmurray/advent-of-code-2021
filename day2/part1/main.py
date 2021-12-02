8aea46dcc771d7f1d3a66b56955f0f511dcacac4
Luke Murray <lukepigeonmail@gmail.com>
2021-12-02 10:15:14 -0500

    day 2 part 1 shorter

--- day2/part1/main.py
+++ day2/part1/main.py
@@ -1,3 +1,3 @@
-x,y,d=0,0,[[t,int(u)]for t,u in[v.split(' ') for v in open('input.txt')]]
+x,y,d=0,0,[[t,int(u)]for t,u in[v.split()for v in open('i')]]
 for t,u in d:x+=u*(t=="forward");y+=u*(t=="down");y-=u*(t=="up")
 print(x*y)
