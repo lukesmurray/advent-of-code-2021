46cb93cb87c9b6a354c19ea3dfb88d04280b69a0
Luke Murray <lukepigeonmail@gmail.com>
2021-12-02 10:17:42 -0500

    day 2 part 2 shorter

--- day2/part2/main.py
+++ day2/part2/main.py
@@ -1,12 +1,3 @@
-with open('input.txt') as f:
-    horizontal = depth = aim = 0
-    d = [[x, int(y)] for x, y in [x.split(' ') for x in f.read().splitlines()]]
-    for direction, amount in d:
-        if direction == "forward":
-            horizontal += amount
-            depth += aim * amount
-        elif direction == "down":
-            aim += amount
-        elif direction == "up":
-            aim -= amount
-    print(horizontal * depth)
+x,y,a,d=0,0,0,[[t,int(u)]for t,u in[v.split()for v in open('i')]]
+for t,u in d:x+=u*(t=="forward");y+=a*u*(t=="forward");a+=u*(t=="down");a-=u*(t=="up")
+print(x*y)
