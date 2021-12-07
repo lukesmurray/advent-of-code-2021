60823f7bb23d24df67d62c3615d3fac10f04d500
Luke Murray <lukepigeonmail@gmail.com>
2021-12-06 22:01:55 -0500

    day 3 part 1

--- day3/part1/main.ts
+++ day3/part1/main.ts
@@ -7,12 +7,19 @@ const parseBinary = R.call(R.curry(parseInt), R.__, 2);
 
 const processInput = R.pipe(
   R.split("\n"),
+  // make an array for each bit
   R.transpose,
+  // count instances of each number
   R.map(R.countBy(R.identity)),
+  // keep the number with the most instances
   R.map(R.pipe(R.toPairs, R.apply(R.maxBy(R.nth(1))), R.nth(0))),
+  // convert the list of numbers to a string
   R.join(""),
+  // multiple gamma/epsilon
   R.converge(R.multiply, [
+    // parse the string as a binary
     parseBinary,
+    // invert the string then parse it as binary
     R.pipe(R.map(Number), R.map(R.subtract(1)), R.join(""), parseBinary),
   ]),
   R.tap(console.log)
