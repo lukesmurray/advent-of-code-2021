61b335c971bc718ee421b303d0976d7b9d0b5aea
Luke Murray <lukepigeonmail@gmail.com>
2021-12-09 14:24:47 -0500

    point free day 8

--- day8/part2/main.ts
+++ day8/part2/main.ts
@@ -4,16 +4,16 @@ import * as R from "ramda";
 R.pipe(
   R.call(R.curry(R.flip(fs.readFileSync)), "utf8"),
   R.pipe(R.split("\n"), R.map(R.split("|")), R.map(R.map(R.trim))),
-  R.map(R.apply(R.useWith(R.merge, [R.objOf("counts"), R.objOf("letters")]))),
+  R.map(R.apply(R.useWith(R.merge, [R.objOf("c"), R.objOf("l")]))),
   R.map(
     R.pipe(
       R.evolve({
-        counts: R.pipe(R.replace(/\s/g, ""), R.countBy(R.identity)),
-        letters: R.pipe(R.split(" "), R.map(R.map(R.identity))),
+        c: R.pipe(R.replace(/\s/g, ""), R.countBy(R.identity)),
+        l: R.pipe(R.split(" "), R.map(R.map(R.identity))),
       }),
       R.converge(R.map, [
-        R.pipe(R.prop("counts"), R.flip(R.prop), R.compose(R.map)),
-        R.prop("letters"),
+        R.pipe(R.prop("c"), R.flip(R.prop), R.compose(R.map)),
+        R.prop("l"),
       ]),
       R.map(R.sum),
       R.map(
