f59af303e817f22143da4df2d3590e2ee5ce840f
Luke Murray <lukepigeonmail@gmail.com>
2021-12-09 14:22:38 -0500

    point free day 8

---
+++ day8/part2/main.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+import * as R from "ramda";
+
+R.pipe(
+  R.call(R.curry(R.flip(fs.readFileSync)), "utf8"),
+  R.pipe(R.split("\n"), R.map(R.split("|")), R.map(R.map(R.trim))),
+  R.map(R.apply(R.useWith(R.merge, [R.objOf("counts"), R.objOf("letters")]))),
+  R.map(
+    R.pipe(
+      R.evolve({
+        counts: R.pipe(R.replace(/\s/g, ""), R.countBy(R.identity)),
+        letters: R.pipe(R.split(" "), R.map(R.map(R.identity))),
+      }),
+      R.converge(R.map, [
+        R.pipe(R.prop("counts"), R.flip(R.prop), R.compose(R.map)),
+        R.prop("letters"),
+      ]),
+      R.map(R.sum),
+      R.map(
+        R.flip(R.prop)({
+          17: 1,
+          25: 7,
+          30: 4,
+          34: 2,
+          37: 5,
+          39: 3,
+          41: 6,
+          42: 0,
+          45: 9,
+          49: 8,
+        })
+      ),
+      R.join(""),
+      Number
+    )
+  ),
+  R.sum,
+  console.log
+)("i");
